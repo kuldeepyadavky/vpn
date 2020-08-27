96a417d06cbcff0a90bd8c266b1c68cc7268eae7
Kuldeep Yadav <ikuldeepyadavky@gmail.com>
2020-08-27 11:03:04 +0530

    UI

---
+++ constants/servers.js
@@ -0,0 +1,38 @@
+import { icons } from "./images";
+
+const servers = [
+  {
+    name: "Automatic",
+    icons: icons.automatic,
+  },
+  {
+    name: "New Delhi",
+    icons: icons.india,
+  },
+  {
+    name: "New York, NY",
+    icons: icons.us,
+  },
+  {
+    name: "Singapore",
+    icons: icons.singapore,
+  },
+  {
+    name: "Melbourne",
+    icons: icons.australia,
+  },
+  {
+    name: "Sweden",
+    icons: icons.sweden,
+  },
+  {
+    name: "London",
+    icons: icons.uk,
+  },
+  {
+    name: "Moscow",
+    icons: icons.russia,
+  },
+];
+
+export default servers;
